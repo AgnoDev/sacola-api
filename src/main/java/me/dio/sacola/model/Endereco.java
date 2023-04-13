ab80b85cc542cb69949a8dc7ecf358a57b983a52
AgnoDev <agnodev87@gmail.com>
2023-04-13 16:44:58 -0300

    Implementa anotações classe Endereco

--- src/main/java/me/dio/sacola/model/Endereco.java
+++ src/main/java/me/dio/sacola/model/Endereco.java
@@ -1,5 +1,18 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor // cria construtor com todos os atributos
+@NoArgsConstructor // cria construtor vazio exigido pelo Hibernate
+@Data // cria getters, setters, equals e hashcode (talvez alguns que nem sejam utilizados)
+@Builder // simplifica criação objetos e auxilia na utilização da classe na camada Service
+@Embeddable // cria os atributos dessa classe dentro de outra tabela e não precisa criar uma tabela específica
 public class Endereco {
 
     private String cep;
