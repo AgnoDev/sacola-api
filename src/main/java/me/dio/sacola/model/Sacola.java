b42a26929aa448a27ffce7ce107ab33f55ada900
AgnoDev <agnodev87@gmail.com>
2023-04-13 16:31:54 -0300

    Implementa anotações classe Sacola

--- src/main/java/me/dio/sacola/model/Sacola.java
+++ src/main/java/me/dio/sacola/model/Sacola.java
@@ -1,11 +1,22 @@
 package me.dio.sacola.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import me.dio.sacola.enumeration.FormaPagto;
 
 import java.util.List;
 
+@AllArgsConstructor // cria construtor com todos os atributos
+@NoArgsConstructor // cria construtor vazio exigido pelo Hibernate
+@Data // cria todos os getters e setters, talvez alguns que nem sejam utilizados
+@Entity // cria tabela no banco de dados
+@Builder // auxilia na utilização da classe na camada Service
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) // ignora erros JSon para atributos Lazy
 public class Sacola {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
