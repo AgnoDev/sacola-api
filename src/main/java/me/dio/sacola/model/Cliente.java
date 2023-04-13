f6e09000e324c698819f66c14ea1be267bfd8ece
AgnoDev <agnodev87@gmail.com>
2023-04-13 16:48:19 -0300

    Implementa anotações classe Cliente

--- src/main/java/me/dio/sacola/model/Cliente.java
+++ src/main/java/me/dio/sacola/model/Cliente.java
@@ -1,9 +1,18 @@
 package me.dio.sacola.model;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor // cria construtor com todos os atributos
+@NoArgsConstructor // cria construtor vazio exigido pelo Hibernate
+@Data // cria getters, setters, equals e hashcode (talvez alguns que nem sejam utilizados)
+@Entity // converte a classe em tabela no banco de dados
+@Builder // simplifica criação objetos e auxilia na utilização da classe na camada Service
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) // ignora erros JSon para atributos Lazy
 
 public class Cliente {
 
@@ -13,7 +22,7 @@ public class Cliente {
 
     private String name;
 
-    @Embedded
+    @Embedded // anotação de atributo: recebe os atributos de outra classe em sua tabela
     private Endereco endereco;
 
 }
