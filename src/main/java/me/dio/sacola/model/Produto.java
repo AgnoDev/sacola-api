aa297c6e2e10da0826942700944cc9016056f86c
AgnoDev <agnodev87@gmail.com>
2023-04-13 16:58:27 -0300

    Implementa anotações classe Produto

--- src/main/java/me/dio/sacola/model/Produto.java
+++ src/main/java/me/dio/sacola/model/Produto.java
@@ -1,8 +1,19 @@
 package me.dio.sacola.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor // cria construtor com todos os atributos
+@NoArgsConstructor // cria construtor vazio exigido pelo Hibernate
+@Data // cria getters, setters, equals e hashcode (talvez alguns que nem sejam utilizados)
+@Entity // converte a classe em tabela no banco de dados
+@Builder // simplifica criação objetos e auxilia na utilização da classe na camada Service
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) // ignora erros JSon para atributos Lazy
 
 public class Produto {
 
@@ -14,10 +25,10 @@ public class Produto {
 
     private Double valorUnitario;
 
-    @Builder.Default
+    @Builder.Default // ao criar o produto, este virá como "disponível" automaticamente
     private Boolean disponivel = true;
 
-    @ManyToOne
+    @ManyToOne // muitos produtos para um restaurante (casse To atributo)
     @JsonIgnore
     private Restaurante restaurante;
 
