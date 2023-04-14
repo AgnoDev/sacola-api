d2faf1a0bbb06d80708b6e67dc3749a929ee6141
AgnoDev <agnodev87@gmail.com>
2023-04-13 21:43:43 -0300

    implementa camada Service (sacolaService)

---
+++ src/main/java/me/dio/sacola/service/SacolaService.java
@@ -0,0 +1,16 @@
+package me.dio.sacola.service;
+
+import me.dio.sacola.dto.ItemDto;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Sacola;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SacolaService {
+
+    Item incluirItem(ItemDto itemDto);
+    Sacola verSacola(Long id);
+    Sacola fecharSacola(Long id, int formaPagto);
+
+
+}
